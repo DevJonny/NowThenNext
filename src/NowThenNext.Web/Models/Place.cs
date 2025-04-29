005ecb8385f8940ef5bcfcdf6227989dfdc96200
Jonny Olliff-Lee <jonnyolliff.lee@wearefreemarket.com>
2025-04-29 21:34:25 +0100

    wIP

--- src/NowThenNext.Web/Models/Place.cs
+++ src/NowThenNext.Web/Models/Place.cs
@@ -1,18 +1,7 @@
-using System.Text.Json;
-
-namespace NowThenNext.Web.Features.PlacesList;
+namespace NowThenNext.Web.Models;
 
 public class Place
 {
-    public Place() { }
-
-    public Place(string name, string description, string group)
-    {
-        Name = name;
-        Description = description;
-        Group = group;
-    }
-    
     public Guid Id { get; init; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
